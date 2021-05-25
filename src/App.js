c1a6b1f9a06b3940ad8f9351ec8d619f03262835
Andrew Moses <andrewmosesdrive@gmail.com>
2021-05-24 22:34:51 -0700

    added reminder status to tasks on double click

--- src/App.js
+++ src/App.js
@@ -8,23 +8,29 @@ function App() {
     {
       text: 'asdfasdafsd',
       id: 1,
-      day: 'blah'
+      day: 'blah',
+      reminder: true
     },
     {
       text: 'asdfasfdsafdsa',
       id: 2,
-      day: 'blah'
+      day: 'blah',
+      reminder: true
     }
   ])
 
   const deleteTask = (id) => {
-    console.log('deleted id: ' + id)
+    setTasks(tasks.filter(task => task.id !== id))
+  }
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map(task => task.id === id ? {...task, reminder: !task.reminder } : task))
   }
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'Fresh outta tasks, yo!'}
     </div>
   );
 }
